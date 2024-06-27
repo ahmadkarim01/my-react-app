33f521602db05d28678793620154494f9b7c45b7
Ahmad Karim <ahmadkarimarbab@gmail.com>
2024-06-27 11:23:39 +0500

    Initial commit

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="path-to-logo" alt="Little Lemon Logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      {/* Main content will go here */}
+    </main>
+  );
+}
+
+export default Main;
